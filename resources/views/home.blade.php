7da6d48434d8e80ab73260d56a6e4d9fba415d3d
Ozias Junior <oziasdias29@gmail.com>
2021-06-07 09:19:49 -0300

    assets, css and php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>DAMON HUBERMAN</title>
+</head>
+
+<body>
+  <div class="structure">
+    <?php include('.\header.blade.php'); ?>
+    <?php include('./main.blade.php'); ?>
+    <?php include('./footer.blade.php'); ?>
+  </div>
+
+
+  <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
