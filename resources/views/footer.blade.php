25a12da2f7ccb3d0e53674f394ee6c97dc92c63f
Ozias Junior <oziasdias29@gmail.com>
2021-06-12 18:36:09 -0300

    css and php

--- resources/views/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer text-white align-items-center d-flex justify-content-between">
-    <p class="p-4">© Damon Huberman - 2021 - Todos os direitos reservados</p>
-    <img class="p-2" src="./assets/image/pawprint.svg" alt="">
-</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,196 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/css/style.css">
-  <title>DAMON HUBERMAN</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/assets/css/style.css">
+    <title>DAMON HUBERMAN</title>
 </head>
 
 <body>
-  <div class="structure">
-    <?php include('.\header.blade.php'); ?>
-    <?php include('./main.blade.php'); ?>
-    <?php include('./footer.blade.php'); ?>
-  </div>
+    <div class="structure">
+        <header class="header">
+            <h1 class="h1">Damon Huberman</h1>
+            <nav class="nav d-flex align-items-center p-3 justify-content-between">
+                <ul class="d-flex">
+                    <li><a href="#" class="btn p-3 text-secondary"><b>Home</b></a></li>
+                    <li><a href="#" class="btn p-3 text-white">Sobre</a></li>
+                    <li><a href="#" class="btn p-3 text-white">Consultoria de Imagem e Estilo</a></li>
+                    <li><a href="#" class="btn p-3 text-white">Contato</a></li>
+                </ul>
+                <form action="" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" aria-label="Search"
+                        placeholder="Search...">
+                </form>
+            </nav>
+            <main class="main">
+                <a href="#" class="img1 d-flex justify-content-end"><img src="./assets/img/img1@1,5x.png" alt=""></a>
+                <a href="#" class="img2"><img src="./assets/imgage/img2@1,5x.png" alt=""></a>
+                <a href="#" class="img3"><img src="./assets/image/img3@1,5x.png" alt=""></a>
+                <div class="left d-flex flex-column">
+                    <div class="title">
+                        <a href="#">
+                            <h2 class="h2 text-dark">Onde se hospedar em SP? Four Seasons</h2>
+                        </a>
+                    </div>
+                    <div class="h6">
+                        <h6 class="mb-20">02/04/2021 • <b>Por Damon • Viagem</b></h6>
+                    </div>
+                    <div class="text">
+                        <p class="text-one">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus cupiditate adipisci
+                            aspernatur placeat quae mollitia quos nobis debitis ratione voluptatem soluta facere
+                            sapiente quod voluptate officia amet consequuntur nostrum harum repudiandae, quidem quis.
+                            Soluta odit ipsam saepe porro ratione facere quasi commodi quam illo eveniet, temporibus
+                            culpa assumenda dicta ex maiores error optio. Magni veniam possimus in vel labore fugit,
+                            repellat tempore dolores quibusdam cupiditate dicta, sit veritatis minus cumque illum. Odio
+                            aut accusantium inventore expedita earum perferendis delectus! Ea libero quas consectetur
+                            doloribus, nobis velit itaque. Magni dolore voluptas rerum eligendi eaque explicabo neque
+                            dolores exercitationem saepe minima ut aliquam assumenda quis pariatur, unde a facilis ipsum
+                            quisquam, eum maiores doloribus! Nostrum ea perferendis reprehenderit. Mollitia odit libero
+                            beatae, officia reprehenderit perspiciatis, nihil nobis sint voluptatibus quos itaque ullam
+                            neque. Rerum consectetur, quisquam praesentium quaerat earum quo laboriosam!
+                        </p>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="damon">
+                        <div>
+                            <img src="./assets/image/p2.jpg" class="rounded mb-4 mx-auto d-block rounded-circle" alt="">
+                        </div>
+                        <div>
+                            <h3>Damon Huberman</h3>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quisquam libero
+                                eos natus praesentium deserunt voluptas, sequi illum placeat sunt corporis consequatur
+                                laudantium et blanditiis tenetur! Ad, reprehenderit. Totam maxime quibusdam architecto
+                                corporis asperiores non distinctio placeat autem eum optio eius, ad provident hic, earum
+                                dolorem. Laborum alias doloremque ducimus!</p>
+                        </div>
+                    </div>
+                    <div class="social d-flex justify-content-center">
+                        <a href="https://www.instagram.com/" target="_blank"><img src="./assets/image/instagram.svg"
+                                alt=""></a>
+                        <a href="https://www.facebook.com/" target="_blank"><img src="./assets/image/facebook.svg"
+                                alt=""></a>
+                        <a href="https://twitter.com/" target="_blank"><img src="./assets/image/twitter.svg" alt=""></a>
+                        <a href="https://br.pinterest.com/" target="_blank"><img src="./assets/image/pinterest.svg"
+                                alt=""></a>
+                        <a href="https://www.youtube.com/" target="_blank"><img src="./assets/image/youtube.svg"
+                                alt=""></a>
+                    </div>
+                </div>
+                <a href="#" class="img4"><img src="./assets/image/img4@1,25x.png" alt=""></a>
+                <div class="category">
+                    <h4>Navegue nas categorias ▼</h4>
+                </div>
+                <div class="subject">
+                    <h4>Navegue nos assuntos ▼</h4>
+                </div>
+                <div>
+                    <h4 class="youtube">YouTube</h4>
+                    <div class="yt mx-auto d-block">
+                        <div class="yt-1" id="___ytsubscribe_4">
+                            <iframe class="yt-iframe" ng-non-bindable="" frameborder="0" hspace="0" marginheight="0"
+                                marginwidth="0" scrolling="no" tabindex="0" vspace="0" width="100%"
+                                id="I4_1621522359824" name="I4_1621522359824"
+                                src="https://www.youtube.com/subscribe_embed?usegapi=1&amp;channel=GoogleDevelopers&amp;theme=dark&amp;layout=full&amp;origin=https%3A%2F%2Fdevelopers-dot-devsite-v2-prod.appspot.com&amp;gsrc=3p&amp;jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.pt_BR.jYaYTbtxFm0.O%2Fam%3DAQ%2Fd%3D1%2Frs%3DAGLTcCNda-zkf9FI4FQ66LQdk16g9ifvmg%2Fm%3D__features__#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh&amp;id=I4_1621522359824&amp;_gfid=I4_1621522359824&amp;parent=https%3A%2F%2Fdevelopers-dot-devsite-v2-prod.appspot.com&amp;pfname=&amp;rpctoken=10173252"
+                                data-gapiattached="true">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h4 class="facebook">Facebook</h4>
+                    <div class="fb-page d-flex justify-content-center" data-href="https://www.facebook.com/facebook"
+                        data-width="500" data-hide-cover="false" data-show-facepile="false">
+                    </div>
+                </div>
+                <div class="text-two">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptas, expedita quod sunt
+                        consectetur asperiores quo ex cumque vero unde quas debitis fuga. Fugiat harum laudantium
+                        aliquid! Quibusdam error odio asperiores itaque voluptas voluptates aut tempore accusamus
+                        molestias! Quidem, consequuntur veniam error cum odio enim, repudiandae aspernatur mollitia
+                        incidunt vel similique obcaecati reiciendis voluptatem quod nobis, quibusdam corporis ullam
+                        accusamus? Quasi tempora provident et, doloribus ipsam optio voluptatibus explicabo.
+                        Reprehenderit debitis officia blanditiis ipsum incidunt unde numquam quod nesciunt ab neque.
+                        Similique perspiciatis veritatis numquam dolore hic explicabo eaque quidem? Repudiandae, dicta.
+                        Commodi minus porro similique accusantium esse necessitatibus sapiente.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, id reiciendis. Debitis
+                        consectetur sunt placeat quas repellat sint aliquam mollitia sapiente sequi totam et illo
+                        dolorem doloremque nulla, modi at iure nostrum ab! Nemo saepe necessitatibus, explicabo, quidem
+                        culpa tempora praesentium dolores nisi molestias nesciunt vitae maiores repellat fugit vero
+                        exercitationem quia dicta eaque deleniti. Velit, maxime. Tempore molestias provident dolores
+                        numquam necessitatibus. Non saepe omnis culpa atque vero eveniet soluta, enim cum pariatur
+                        similique cupiditate. Deleniti, cumque unde. Nobis ratione eaque voluptates alias quae, corporis
+                        voluptate eius blanditiis cum cupiditate, amet earum culpa labore rerum veritatis. Totam,
+                        laborum corporis?</p>
+                </div>
+                <div class="instagram">
+                    <h4>My Instagram</h4>
+                </div>
+                <div class="inst">
+                    <a href="https://www.instagram.com/" target="_blank"><img src="./assets/image/instagram.svg
+                " alt=""></a>
+                    <div class="img-instagram">
+                        <div class="img-one d-flex justify-between">
+                            <a href="https://instagram.com/" target="_blank"><img src="./assets/image/instagram01.jpg"
+                                    alt=""></a>
+                            <a href="https://instagram.com/" target="_blank"><img src="./assets/image/instagram02.jpg"
+                                    alt=""></a>
+                            <a href="https://instagram.com/" target="_blank"><img src="./assets/image/instagram03.jpg"
+                                    alt=""></a>
+                        </div>
+                        <div class="img-two d-flex justify-between">
+                            <a href="https://instagram.com/" target="_blank"><img src="./assets/image/instagram04.jpg"
+                                    alt=""></a>
+                            <a href="https://instagram.com/" target="_blank"><img src="./assets/image/instagram05.jpg"
+                                    alt=""></a>
+                            <a href="https://instagram.com/" target="_blank"><img src="./assets/image/instagram06.jpg"
+                                    alt=""></a>
+                        </div>
+                        <button type="button" class="btn p-3 btn-primary d-flex mx-auto">Siga-me no Instagram</button>
+                    </div>
+                </div>
+                <div class="col-wide end">
+                    <div>
+                        <p class="col-wide"><b>Sem comentários</b> • Compartilhe:</p>
+                    </div>
+                    <div class="send">
+                        <div class="col-wide">
+                            <a href="https://www.facebook.com/" target="_blank"><img src="./assets/image/facebook.svg"
+                                    alt=""></a>
+                            <a href="https://twitter.com/" target="_blank"><img src="./assets/image/twitter.svg"
+                                    alt=""></a>
+                            <a href="#" target="_blank"><img src="./assets/image/whatsapp.svg" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="read">
+                    <h5 class="col-wide">Aproveite para ler também:</h5>
+                </div>
+                <div>
+                    <a href="#"><img class="img-fluid read-img01" src="./assets/image/img5@1,25x.png" alt=""></a>
+                </div>
+                <div>
+                    <a href="#"><img class="img-fluid read-img02" src="./assets/image/img6@1,25x.png" alt=""></a>
+                </div>
+            </main>
+        </header>
+        <footer class="footer text-white align-items-center d-flex justify-content-between">
+            <p class="p-4">© Damon Huberman - 2021 - Todos os direitos reservados</p>
+            <img class="p-2" src="./assets/image/pawprint.svg" alt="">
+        </footer>
+    </div>
 
 
-  <script src="./assets/js/script.js"></script>
+    <script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v10.0&appId=284615029380357&autoLogAppEvents=1"
+        nonce="yjSVpQ6O"></script>
+    <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
